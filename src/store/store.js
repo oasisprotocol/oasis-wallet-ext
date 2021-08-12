77a84424303b839404d495932d8983eb4693883e
lukaw3d <lukajeran@oasislabs.com>
2021-08-12 03:26:55 +0200

    Remove unused store.js

--- src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { applyMiddleware, createStore } from "redux";
-import thunk from 'redux-thunk';
-import rootReducer from '../reducers';
-
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-export default store;
