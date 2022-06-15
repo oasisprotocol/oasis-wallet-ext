f7c1d07ab92e3c289abf72312a94a1b572ee9f23
lukaw3d <lukajeran@oasislabs.com>
2022-06-15 03:05:42 +0200

    Add button component type

--- src/popup/component/Button/index.js
+++ src/popup/component/Button/index.js
@@ -6,10 +6,27 @@ export const BUTTON_TYPE_CANCEL = "BUTTON_TYPE_CANCEL"
 export const BUTTON_TYPE_CONFIRM = "BUTTON_TYPE_CONFIRM"
 export const BUTTON_TYPE_HOME_BUTTON = "BUTTON_TYPE_HOME_BUTTON"
 export const BUTTON_TYPE_COMMON_BUTTON = "BUTTON_TYPE_COMMON_BUTTON"
-
-
-
 export const BUTTON_TYPE_SMALL = "BUTTON_TYPE_SMALL"
+
+/**
+ * @typedef Props
+ * @prop {React.ReactNode} content
+ * @prop {React.MouseEventHandler<HTMLButtonElement>} onClick
+ * @prop {boolean} [disabled]
+ * @prop {string} [propsClass]
+ * @prop {(
+ *  | BUTTON_TYPE_CANCEL
+ *  | BUTTON_TYPE_CONFIRM
+ *  | BUTTON_TYPE_HOME_BUTTON
+ *  | BUTTON_TYPE_COMMON_BUTTON
+ *  | BUTTON_TYPE_SMALL
+ * )} [buttonType]
+ * @prop {boolean} [imgLeft]
+ */
+
+/**
+ * @extends Component<Props>
+ */
 export default class Button extends Component {
     constructor(props) {
         super(props);
@@ -23,9 +40,7 @@ export default class Button extends Component {
         switch (buttonType) {
             case BUTTON_TYPE_COMMON_BUTTON:
             default:
-                currentClassName = cx("click-cursor",{
-                    "common-button": true,
-                    [propsClass]: true,
+                currentClassName = cx("common-button", propsClass, {
                     "click-cursor": !disabled,
                     "click-cursor-disable": disabled,
                 })
